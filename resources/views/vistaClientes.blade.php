4568994e3d2e58c61e415eb16a3b4ffb1d4bdaf6
Miquel <6692.joan23@fje.edu>
2022-02-08 20:48:08 +0100

    sessiones + ajax

--- resources/views/vistaClientes.blade.php
+++ resources/views/vistaClientes.blade.php
@@ -8,6 +8,7 @@
     <script type="text/javascript" src="js/iconos_g.js"></script> <!-- iconos FontAwesome-->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> <!-- jquery-->
     <script src="js/js.js"></script>
+    <script src="js/js.js"></script>
     <script src="js/ajax.js"></script>
     <link rel="stylesheet" href="css/style.css">
     <title>Deliveroo - Food Delivery</title>
